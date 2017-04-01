1db4fb93a05da8ad01ed8f3da3ad5d589a06b3ee
Robin Hill <rehill012@gmail.com>
2017-04-01 11:26:04 -0400

    Switch to creds json file for security

--- bookbot.py
+++ bookbot.py
@@ -1,12 +1,8 @@
 import bookgen
-from BotBuddy import BotBuddy
+from botbuddy import BotBuddy
 
 credentials = {
-    
-    BotBuddy.consumer_key_key : "W8mx3eg7mA4J8pZjSE3gkPplN",
-    BotBuddy.consumer_secret_key : "iHh2ZWXcTrRtbToTV8q1QdkdsEZ9KIjsN7RgonfzOsyheyZkjt",
-    BotBuddy.access_token_key : "798422658425778176-at7rlU1vIugbbZ3hvvVIVLXvV93tNKB",
-    BotBuddy.access_token_secret_key : "N7ZlfS2yVI3JFNsO0osPz6VewV4KVXS6O0mhy9nCvFHKi"
+    BotBuddy.creds_file_key = "creds.json"
 }
     
 body = BotBuddy()
